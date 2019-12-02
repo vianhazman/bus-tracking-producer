c863043d5bca1bd105e4cde72f562e0c2eee295f
vianhazman <vianhazman@gmail.com>
2019-12-02 12:53:29 +0700

    [DURATION] Change duration from 5s to 10s

--- app/nrt_5s_tj_location.py
+++ app/nrt_5s_tj_location.py
@@ -5,7 +5,7 @@ from datetime import timedelta
 default_args = {
     'owner': 'Airflow',
     'depends_on_past': False,
-    'start_date': airflow.utils.dates.days_ago(2),
+    'start_date': airflow.utils.dates.days_ago(0),
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
@@ -13,7 +13,7 @@ default_args = {
     'retry_delay': timedelta(seconds=20)
 }
 dag1 = DAG(
-    'nrt_tj_stream', default_args=default_args, schedule_interval=timedelta(seconds=5))
+    'nrt_tj_stream',  catchup=False, default_args=default_args, schedule_interval=timedelta(seconds=10))
 
 bash_task = BashOperator(
     task_id='5s_tracking_data',
